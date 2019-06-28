ec8748b22f6cc788073dc0e3b5898ddbb03e0a27
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2019-06-28 13:24:44 +0700

    new module berita n pencarian

---
+++ resources/views/pencarian/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container" style="margin-top: 10em; margin-bottom: 4em;">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card shadow">
+                    <div class="card-header">
+                        Halaman Pencarian Universitas
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
