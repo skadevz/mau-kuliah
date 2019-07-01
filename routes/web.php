b51b9972611d9718d7e3ad76fd61e81a2fcbcb28
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2019-07-01 16:41:36 +0700

    update web add universitas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,21 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('berita', 'BeritaController@index')->name('berita.index');
 Route::get('search', 'PencarianController@index')->name('pencarian.index');
 Route::get('peringkat-universitas', 'PeringkatUniversitasController@index')->name('peringkat_universitas');
+
+
+// Admin Page
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
+    Route::get('dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::group(['prefix' => 'master'], function () {
+        Route::group(['prefix' => 'universitas'], function () {
+            Route::get('/', 'Master\UniversitasController@index')->name('universitas.index');
+            Route::get('create', 'Master\UniversitasController@create')->name('universitas.create');
+            Route::post('store', 'Master\UniversitasController@store')->name('universitas.store');
+            Route::get('{id}/edit', 'Master\UniversitasController@edit')->name('universitas.edit');
+            Route::post('update', 'Master\UniversitasController@update')->name('universitas.update');
+            Route::get('{id}/delete', 'Master\UniversitasController@delete')->name('universitas.delete');
+        });
+    });
+});
