7174b69c1a994ad07c4665cc8118c20b2d6e07bf
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2019-07-01 11:27:38 +0700

    user migration and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(UniversitasTableSeeder::class);
         $this->call(JurusanTableSeeder::class);
         $this->call(JenjangTableSeeder::class);
