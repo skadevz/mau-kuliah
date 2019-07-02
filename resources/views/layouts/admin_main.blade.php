34b72c70aa44b5876e81d1ff87bfc65fbcc52b22
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2019-07-02 13:47:30 +0700

    add default kampus image

--- resources/views/layouts/admin_main.blade.php
+++ resources/views/layouts/admin_main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>MAU KULIAH</title>
-    <link rel="icon" href="{{ asset('assets/img/ico.ico') }}">
+    <link rel="icon" href="{{ asset('assets/img/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/admin/dashboard.css')}}" />
     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" />
